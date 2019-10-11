8a8eb1710369160a3addae9a333a89f8971343f4
James Benjamin <james.abenj@gmail.com>
2019-10-11 12:48:50 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,13 +2,9 @@ class Post
   
   attr_reader :title, :author 
   
-  def initialize(title, author)
+  def initialize(title)
     @title = title
-    @author = author
   end 
-  
-  def author
-    @author
-  end 
+
   
 end 
